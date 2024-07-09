2d92cdbe13ea5ed159b3d9999430053e05c6b638
Vahe Sevachyan <vscode13@gmail.com>
2024-07-08 22:01:50 -0700

    preparing startupfiles

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup></script>
+
+<template>
+  <main>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+      <div class="cards-container">
+        <div class="card">
+          <p class="main-text"></p>
+          <p class="date"></p>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+}
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+}
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+h1 {
+  font-weight: bold;
+  margin-bottom: 25px;
+  font-size: 45px;
+}
+button {
+  border: none;
+  padding: 10px;
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  background-color: black;
+  border-radius: 100%;
+  color: white;
+  font-size: 18px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
