bc11fc53d5dea8f6968126b32fb2c5ffb978c50f
Vahe Sevachyan <vscode13@gmail.com>
2024-07-10 22:45:45 -0700

    created EditModal Component

--- src/components/EditModal.vue
+++ src/components/EditModal.vue
@@ -1,4 +1,42 @@
-<template></template>
+<template>
+  <div
+    v-bind:class="['card', { expanded: isCardExpanded }]"
+    v-bind:style="{ backgroundColor: note.cards.backgroundColor }"
+  >
+    <!-- backGroundImage -->
+    <div class="card-title-container">
+      <p>{{ note.title }}</p>
+      <ul class="editBtn">
+        <li @click="editNote"><img src="../assets/edit.svg" /></li>
+        <li @click="deleteNote"><img src="../assets/trash.svg" /></li>
+      </ul>
+    </div>
+    <!--No styles for text-container -->
+    <div class="text-container">
+      <p v-if="isTextShowing" class="main-text">{{ note.cards.text }}</p>
+      <p v-else>...</p>
+    </div>
+    <div class="btn-container">
+      <button
+        class="toggle-card-size-btn"
+        @click="toggleCardSize"
+        v-bind:style="{ backgroundColor: note.cards.btnColor }"
+      >
+        <span v-if="!isCardExpanded">
+          <img src="../assets/arrow-down.svg" alt="" />
+          Expand
+        </span>
+        <span v-else>
+          <img src="../assets/arrow-up.svg" alt="" /> Collapse</span
+        >
+      </button>
+      <button class="toggle-text-hide-btn" @click="toggleCardHide">
+        {{ HideBtnText }}
+      </button>
+    </div>
+    <p class="date">{{ note.cards[0].date.toLocaleString("en-US") }}</p>
+  </div>
+</template>
 
 <script></script>
 <style></style>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="over-lay">
     <div class="modal">
-      <h2 class="modalTitle">{{ title }}</h2>
+      <h2 class="modalTitle">{{ newNoteTitle }}</h2>
       <input
         type="text"
         class="title-input"
@@ -53,5 +53,119 @@ function saveNote() {
   titleText.value = "";
   emit("add-note", newNote);
 }
+function closeModal() {
+  noteText.value = "";
+  titleText.value = "";
+  errorMessage.value = "";
+  emit("close");
+}
+function getRandomColor() {
+  return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+}
 </script>
-<style scoped></style>
+<style scoped>
+.title-input {
+  border: 1px solid black;
+  margin-bottom: 10px;
+  width: 100%;
+}
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.77);
+  z-index: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.modalTitle {
+  margin-top: -15px;
+  margin-bottom: 15px;
+  text-align: center;
+  font-family: "Nunito", Verdana, sans-serif;
+  font-size: 25px;
+  font-weight: bold;
+}
+.title-input:focus {
+  outline: none;
+  border: 2px solid #106de6;
+}
+.modal {
+  width: 450px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  padding: 30px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.modal button {
+  background-image: linear-gradient(
+    to right,
+    #24c6dc 0%,
+    #514a9d 51%,
+    #24c6dc 100%
+  );
+  width: 100%;
+  margin-top: 15px;
+  padding: 15px 45px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  box-shadow: 0 0 20px #eee;
+  border-radius: 10px;
+  border: none;
+  display: block;
+}
+
+.modal button:hover {
+  background-position: right center; /* change the direction of the change here */
+  color: #fff;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+#close {
+  background-image: linear-gradient(
+    to right,
+    #dc2424 0%,
+    #4a569d 51%,
+    #dc2424 100%
+  );
+  margin-top: 15px;
+  padding: 15px 45px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  box-shadow: 0 0 20px #eee;
+  border-radius: 10px;
+  display: block;
+}
+
+#close:hover {
+  background-position: right center; /* change the direction of the change here */
+  color: #fff;
+  text-decoration: none;
+}
+
+.errorMsg {
+  color: tomato;
+  font-family: Verdana, Tahoma, sans-serif;
+}
+
+textarea {
+  resize: none;
+  border: 1.8px solid black;
+  width: 100%;
+}
+textarea:focus {
+  outline: none;
+  border: 2px solid #106de6;
+}
+</style>
